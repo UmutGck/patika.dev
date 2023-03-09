b61afb0b9e5d5d9dddae2137ff665e7b93740a29
doguozcan <126794820+doguozcan@users.noreply.github.com>
2023-03-09 13:18:10 +0300

    Create DortVeBesinKuvvetleri.java

---
+++ java101/DortVeBesinKuvvetleri.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class DortVeBesinKuvvetleri {
+    public static void main(String[] args) {
+        int n;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Lütfen sayıyı giriniz: ");
+        n = input.nextInt();
+
+        for (int i = 1, j = 1; i <= n && j <= n; i *= 4, j *= 5) {
+            System.out.println(i);
+            System.out.println(j);
+        }
+    }
+}
